606ebf788c31da9916715295b321adb9cef6cb0d
Bart Noordervliet <bart@mmvi.nl>
2025-01-19 14:18:27 +0100

    Add "gethist" websocket request to get run history

--- src/web.rs
+++ src/web.rs
@@ -3,7 +3,7 @@ use futures_util::{SinkExt as _, StreamExt};
 use hyper::body;
 use hyper_util::rt::TokioIo;
 use serde::{Deserialize, Serialize};
-use tokio::{ net::TcpListener, sync::broadcast };
+use tokio::{ io::AsyncReadExt, net::TcpListener, sync::broadcast };
 
 use crate::{ Config, Job, JsonJob, Schedule };
 
@@ -63,6 +63,19 @@ pub async fn handle_websocket(ws: hyper_tungstenite::HyperWebsocket, config: Arc
         log: String,
         err: String
     }
+    #[derive(Default, Serialize)]
+    struct JsonHistory {
+        msg: &'static str,
+        path: String,
+        runs: Vec<JsonRun>
+    }
+    #[derive(Default, Serialize)]
+    struct JsonRun {
+        ts: String,
+        status: u8,
+        log: String,
+        err: String
+    }
 
     let (mut ws_tx, mut ws_rx) = ws.await?.split();
 
@@ -90,7 +103,7 @@ pub async fn handle_websocket(ws: hyper_tungstenite::HyperWebsocket, config: Arc
                                                 ws_tx.send(hyper_tungstenite::tungstenite::Message::Text(serde_json::to_string(&res)?.into())).await?;
                                             },
                                             text => {
-                                                let req: JsonReq = serde_json::from_str(text)?;
+                                                let req: JsonReq = serde_json::from_str(text)?; // Returns from handler (closing the ws) on invalid JsonReq
                                                 match req.req.as_str() {
                                                     "getlog" => {
                                                         let mut res = None;
@@ -111,6 +124,68 @@ pub async fn handle_websocket(ws: hyper_tungstenite::HyperWebsocket, config: Arc
                                                             ws_tx.send(hyper_tungstenite::tungstenite::Message::Text(serde_json::to_string(&res)?.into())).await?
                                                         }
                                                     },
+                                                    "gethist" => {
+                                                        let mut res = JsonHistory { msg: "hist", path: req.path.clone(), runs: vec![] };
+                                                        let dirpath = match config.read() {
+                                                            Ok(config) => config.dir.join(req.path).join("runs"),
+                                                            Err(_) => continue
+                                                        };
+                                                        if !dirpath.is_dir() { continue; }
+                                                        match tokio::fs::read_dir(&dirpath).await {
+                                                            Ok(mut dir) => {
+                                                                while let Ok(Some(entry)) = dir.next_entry().await {
+                                                                    match entry.file_type().await {
+                                                                        Ok(ftype) => {
+                                                                            if !ftype.is_dir() { continue; }
+                                                                        },
+                                                                        Err(_) => continue
+                                                                    }
+                                                                    let path = dirpath.join(entry.path());
+                                                                    let mut run = JsonRun { ts: entry.file_name().to_string_lossy().to_string(), ..Default::default() };
+                                                                    run.status = match tokio::fs::File::open(path.join("status")).await {
+                                                                        Ok(mut file) => {
+                                                                            let mut status = String::new();
+                                                                            if let Err(e) = file.read_to_string(&mut status).await {
+                                                                                eprintln!("Failed to read {}/status even though it exists: {}", path.display(), e);
+                                                                                continue;
+                                                                            }
+                                                                            if status.len() < 2 { // Statusfile should contain at least one digit and a newline
+                                                                                eprintln!("Status file in {} is invalid", path.display());
+                                                                                continue;
+                                                                            }
+                                                                            match status[..status.len()-1].parse::<u8>() {
+                                                                                Ok(value) => value,
+                                                                                Err(e) => {
+                                                                                    eprintln!("Status file in {} did not contain a valid 8-bit integer: {}", path.display(), e);
+                                                                                    continue;
+                                                                                }
+                                                                            }
+                                                                        },
+                                                                        Err(e) => {
+                                                                            eprintln!("Failed to read {}/status: {}", path.display(), e);
+                                                                            continue;
+                                                                        }
+                                                                    };
+                                                                    if let Ok(mut file) = tokio::fs::File::open(path.join("out")).await {
+                                                                        if let Err(e) = file.read_to_string(&mut run.log).await {
+                                                                            eprintln!("Failed to read {}/out even though it exists: {}", path.display(), e);
+                                                                        }
+                                                                    };
+                                                                    if let Ok(mut file) = tokio::fs::File::open(path.join("err")).await {
+                                                                        if let Err(e) = file.read_to_string(&mut run.err).await {
+                                                                            eprintln!("Failed to read {}/err even though it exists: {}", path.display(), e);
+                                                                        }
+                                                                    };
+                                                                    res.runs.push(run);
+                                                                }
+                                                            },
+                                                            Err(e) => {
+                                                                eprintln!("Error while opening runs dir {}: {}", dirpath.display(), e);
+                                                                continue;
+                                                            }
+                                                        }
+                                                        ws_tx.send(hyper_tungstenite::tungstenite::Message::Text(serde_json::to_string(&res)?.into())).await?
+                                                    },
                                                     _ => {
                                                         println!("Received unexpected websocket message: {}", text);
                                                         continue;
@@ -123,7 +198,7 @@ pub async fn handle_websocket(ws: hyper_tungstenite::HyperWebsocket, config: Arc
                                     other => println!("Received unexpected websocket message: {:?}", other)
                                 }
                             },
-                            Err(e) => println!("Received websocket error: {}", e)
+                            Err(_) => {} // println!("Received websocket error: {}", e)
                         }
                     },
                     None => break // Websocket closed
