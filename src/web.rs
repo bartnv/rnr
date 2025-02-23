33d840c7924b6b43df5ae4e7c11b1f1c8c5ec76e
Bart Noordervliet <bart@mmvi.nl>
2025-02-23 21:38:01 +0100

    Add http api endpoint to fetch the jobfile contents

--- src/web.rs
+++ src/web.rs
@@ -1,9 +1,9 @@
 use crate::{ Config, Job, JsonJob, JsonRun };
-use std::{ convert::Infallible, sync::{ Arc, RwLock } };
+use std::{ convert::Infallible, path::PathBuf, sync::{ Arc, RwLock } };
 use axum::{ extract::{ Path, State }, http::StatusCode, response::sse::{ Event, KeepAlive, Sse }, routing::{ get, post }, Json, Router };
 use futures::Stream;
 use serde::Serialize;
-use tokio::{io::AsyncReadExt as _, sync::broadcast};
+use tokio::{ fs, io::AsyncReadExt as _, sync::broadcast };
 use async_stream::try_stream;
 use tower_http::services::ServeFile;
 
@@ -17,20 +17,21 @@ pub async fn run(config: Arc<RwLock<Config>>, broadcast: broadcast::Sender<Job>)
     let app = Router::new()
         .route_service("/", ServeFile::new("rnr/web/index.html"))
         .route_service("/favicon.ico", ServeFile::new("rnr/web/favicon.ico"))
-        .route("/jobs", get(jobs))
-        .route("/jobs/{path}", get(job))
-        .route("/jobs/{path}/runs", get(runs))
-        .route("/updates", get(updates))
+        .route("/jobs", get(get_jobs))
+        .route("/jobs/{path}", get(get_job))
+        .route("/jobs/{path}/runs", get(get_runs))
+        .route("/jobs/{path}/config", get(get_config))
+        .route("/updates", get(get_updates))
         .with_state(AppState { config, broadcast });
     let listener = tokio::net::TcpListener::bind("0.0.0.0:1234").await.unwrap();
     axum::serve(listener, app).await.unwrap()
 }
 
-async fn jobs(State(state): State<AppState>) -> Json<Vec<JsonJob>> {
+async fn get_jobs(State(state): State<AppState>) -> Json<Vec<JsonJob>> {
     Json(state.config.read().unwrap().jobs.values().map(|j| j.to_json()).collect())
 }
 
-async fn job(State(state): State<AppState>, Path(path): Path<String>) -> Result<Json<JsonRun>, StatusCode> {
+async fn get_job(State(state): State<AppState>, Path(path): Path<String>) -> Result<Json<JsonRun>, StatusCode> {
     if let Some(job) = state.config.read().unwrap().jobs.get(&path) {
         if let Some(lastrun) = &job.lastrun {
             return Ok(Json(lastrun.to_json()));
@@ -39,7 +40,7 @@ async fn job(State(state): State<AppState>, Path(path): Path<String>) -> Result<
     Err(StatusCode::NOT_FOUND)
 }
 
-async fn runs(State(state): State<AppState>, Path(path): Path<String>) -> Result<Json<Vec<JsonRun>>, StatusCode> {
+async fn get_runs(State(state): State<AppState>, Path(path): Path<String>) -> Result<Json<Vec<JsonRun>>, StatusCode> {
     let dirpath = match state.config.read() {
         Ok(config) => config.dir.join(path).join("runs"),
         Err(_) => return Err(StatusCode::NOT_FOUND)
@@ -103,7 +104,33 @@ async fn runs(State(state): State<AppState>, Path(path): Path<String>) -> Result
     Ok(Json(res))
 }
 
-async fn updates(State(state): State<AppState>) -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
+async fn get_config(State(state): State<AppState>, Path(path): Path<String>) -> Result<String, StatusCode> {
+    let filename = {
+        let rconfig = state.config.read().unwrap();
+        if !rconfig.jobs.contains_key(&path) {
+            eprintln!("Invalid job path requested on config endpoint: {}", path);
+            return Err(StatusCode::BAD_REQUEST);
+        }
+        rconfig.dir.join(&path).join("job.yml").to_path_buf()
+    };
+    let jobfile = match fs::File::open(filename).await {
+        Ok(mut file) => {
+            let mut contents = vec![];
+            if let Err(e) = file.read_to_end(&mut contents).await {
+                eprintln!("Failed to read job.yml in directory \"{}\": {}", path, e);
+                return Err(StatusCode::INTERNAL_SERVER_ERROR);
+            }
+            String::from_utf8_lossy(&contents).into_owned()
+        }
+        Err(_) => {
+            eprintln!("Job path {} requested on config endpoint has no job.yml file", path);
+            return Err(StatusCode::NOT_FOUND);
+        }
+    };
+    Ok(jobfile)
+}
+
+async fn get_updates(State(state): State<AppState>) -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
     let mut rx = state.broadcast.subscribe();
     Sse::new(try_stream! {
         yield Event::default();
